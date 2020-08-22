8f856f26f0ee3f7857dfdcc65c8c3ab4b446a025
binhaizhijun <binhaizhijun@163.com>
2020-08-22 17:04:07 +0800

    添加到GitHub的代码结构

---
+++ src/main/java/myProject.java
@@ -0,0 +1,13 @@
+/**
+ * @ProjectName: myProject
+ * @Package: PACKAGE_NAME
+ * @ClassName: myProject
+ * @Description: java类作用描述
+ * @Author: 滨海之君
+ * @CreateDate: 2020/8/22 16:37
+ */
+public class myProject {
+    public static void main(String[] args) {
+        System.out.println("这是我的第一个项目");
+    }
+}
